93c2490a3eb58af56d71cff81949048b0c0c2023
Piotr Marendowski <piotr-marendowski@tutanota.com>
2024-02-12 18:03:55 +0100

    Aktualizacja algorytmy/1/a.py

--- algorytmy/1/a.py
+++ algorytmy/1/a.py
@@ -1,12 +1,7 @@
 # Piotr Marendowski, GNU GPL v.3
+# Konwersja systemow liczbowych
 
-# g) HEX -> BIN,
-# h) BIN -> HEX,
-# i) OCT -> BIN,
-# j) BIN -> OCT
-# k) DEC -> p (dowolny)
-
-
+# Uzywajac wbudowanych funkji
 # a) DEC -> BIN,
 def dec_to_bin(dec_list):
     bin_list = []
@@ -70,7 +65,7 @@ def dec_to_oct(dec_list):
 
     for num in dec_list:
         oct_list.append(oct(num)[2:])
-        # Mozna tez konwertowac w trakcie wtf (linked listy to moga byc po prostu)
+        # Mozna tez konwertowac w trakcie wtf (linked listy to sa pewnie)
         str(num)
 
     print("DEC -> OCT:")
@@ -87,22 +82,90 @@ def oct_to_dec(oct_list):
     for num in oct_list:
         print(int(num, 8), end="  ")
 
+    # Przez ten `end=` musi to byc zeby nastepny print by w nowej linii
+    print("")
 
-dec_list = [0, 1, 2, 4, 5, 6, 10, 11, 18, 19, 25, 37, 44]
-# przekonwertuj liste intow na liste stringow i wyswietl bez '[' i '''
-dec_str_list = [str(num) for num in dec_list]
-print("Oryginalna lista:")
-print("  ".join(dec_str_list), "\n")
 
-# a) DEC -> BIN,
-bin_list = dec_to_bin(dec_list)
-# b) BIN -> DEC,
-bin_to_dec(bin_list)
-# c) DEC -> HEX,
-hex_list = dec_to_hex(dec_list)
-# d) HEX -> DEC,
-hex_to_dec(hex_list)
-# e) DEC -> OCT,
-oct_list = dec_to_oct(dec_list)
-# f) OCT -> DEC,
-oct_to_dec(oct_list)
+# g) HEX -> BIN,
+def hex_to_bin(hex_list):
+    print("HEX -> BIN:")
+
+    for num in hex_list:
+        print(bin(int(num, 16))[2:], end="  ")
+
+    print("")
+
+
+# h) BIN -> HEX,
+def bin_to_hex(bin_list):
+    print("BIN -> HEX:")
+
+    for num in bin_list:
+        print(hex(int(num, 2))[2:], end="  ")
+
+    print("")
+
+
+# i) OCT -> BIN,
+def oct_to_bin(oct_list):
+    print("OCT -> BIN:")
+
+    for num in oct_list:
+        print(bin(int(num, 8))[2:], end="  ")
+
+    print("")
+
+
+# j) BIN -> OCT
+def bin_to_oct(bin_list):
+    print("BIN -> OCT:")
+
+    for num in bin_list:
+        print(oct(int(num, 2))[2:], end="  ")
+
+    print("")
+
+
+# k) DEC -> p (dowolny)
+def dec_to_p(dec_list):
+    print("DEC -> p (dowolny):")
+
+
+def main():
+    dec_list = [0, 1, 2, 4, 5, 6, 10, 11, 18, 19, 25, 37, 44]
+    # przekonwertuj liste intow na liste stringow i wyswietl bez '[' i '''
+    dec_str_list = [str(num) for num in dec_list]
+    print("Oryginalna lista:")
+    print("  ".join(dec_str_list), "\n")
+
+    # Uzywajac wbudowanych funkji
+    print("Uzywajac wbudowanych funkji:")
+    # a) DEC -> BIN,
+    bin_list = dec_to_bin(dec_list)
+    # b) BIN -> DEC,
+    bin_to_dec(bin_list)
+    # c) DEC -> HEX,
+    hex_list = dec_to_hex(dec_list)
+    # d) HEX -> DEC,
+    hex_to_dec(hex_list)
+    # e) DEC -> OCT,
+    oct_list = dec_to_oct(dec_list)
+    # f) OCT -> DEC,
+    oct_to_dec(oct_list)
+    # g) HEX -> BIN,
+    hex_to_bin(hex_list)
+    # h) BIN -> HEX,
+    bin_to_hex(bin_list)
+    # i) OCT -> BIN,
+    oct_to_bin(oct_list)
+    # j) BIN -> OCT
+    bin_to_oct(bin_list)
+    # k) DEC -> p (dowolny)
+    dec_to_p(dec_list)
+
+
+    # Uzywajac wlasnych funkji konwersji
+
+
+if __name__ == "__main__":
+    main()
